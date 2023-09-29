75fbaf477c063c74061eafcf78fa72c1a0ef6403
Azikenp <azikenp26@gmail.com>
2023-09-29 17:02:36 +0100

    migrating assets to public

--- data.js
+++ data.js
@@ -1,30 +1,30 @@
 export const data = [
     {
-        src: "./src/assets/dictionary.png",
+        src: "./assets/dictionary.png",
         text: 'A dictionary app',
         liveSite: 'https://dictionary-react-azikenp.vercel.app/',
         codeSite: 'https://github.com/Azikenp/dictionary-react.git'
     },
     {
-        src: "./src/assets/weather.png",
+        src: "./assets/weather.png",
         text: 'A mini weather app.',
         liveSite: 'https://weather-app-azikenp.vercel.app/',
         codeSite: 'https://github.com/Azikenp/weather-app.git'
     },
     {
-        src: "./src/assets/hackerNews.png",
+        src: "./assets/hackerNews.png",
         text: 'Get real-time news updates',
         liveSite: 'https://news-update-azikenp.vercel.app/',
         codeSite: 'https://github.com/Azikenp/news-update.git'
     },
     {
-        src: "./src/assets/crypto-zazoo.png",
+        src: "./assets/crypto-zazoo.png",
         text: 'A crypto updates app',
         liveSite: 'https://crypto-bucks-azikenp.vercel.app/',
         codeSite: 'https://github.com/Azikenp/crypto-bucks.git'
     },
     {
-        src: "./src/assets/bookhub.png",
+        src: "./assets/bookhub.png",
         text: 'A book search app',
         liveSite: 'https://book-library-azikenp.vercel.app/',
         codeSite: 'https://github.com/Azikenp/book-library.git'
